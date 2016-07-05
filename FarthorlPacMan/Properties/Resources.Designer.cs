caf715ac3a226ae329b0d6e63262ecdcfaae3cf8
DonchoPatronski <donstz@yahoo.com>
2016-07-05 19:17:51 +0300

    Location on images moved to DataFiles

--- FarthorlPacMan/Properties/Resources.Designer.cs
+++ FarthorlPacMan/Properties/Resources.Designer.cs
@@ -113,16 +113,6 @@ namespace FarthorlPacMan.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap LabirintImage1 {
-            get {
-                object obj = ResourceManager.GetObject("LabirintImage1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap MenuBackGround {
             get {
                 object obj = ResourceManager.GetObject("MenuBackGround", resourceCulture);
