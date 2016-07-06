0bc7728e40f0c922d26a5ca085005324af424ac1
IvayloKodov <Ivailo_Kodov@abv.bg>
2016-07-06 11:41:20 +0300

    .

--- FarthorlPacMan/States/LevelsWindow.Designer.cs
+++ FarthorlPacMan/States/LevelsWindow.Designer.cs
@@ -39,52 +39,52 @@
             // 
             // Labirint_Level
             // 
+            this.Labirint_Level.BackgroundImage = global::FarthorlPacMan.Properties.Resources.Labirint_Button;
             this.Labirint_Level.Font = new System.Drawing.Font("Snap ITC", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Labirint_Level.ForeColor = System.Drawing.Color.Maroon;
             this.Labirint_Level.Location = new System.Drawing.Point(398, 30);
             this.Labirint_Level.Name = "Labirint_Level";
             this.Labirint_Level.Size = new System.Drawing.Size(174, 54);
             this.Labirint_Level.TabIndex = 0;
-            this.Labirint_Level.Text = "LABIRINT";
             this.Labirint_Level.UseVisualStyleBackColor = true;
             this.Labirint_Level.Click += new System.EventHandler(this.Labirint_Level_Click);
             this.Labirint_Level.MouseHover += new System.EventHandler(this.Labirint_Level_MouseHover);
             // 
             // CsharpLevel
             // 
+            this.CsharpLevel.BackgroundImage = global::FarthorlPacMan.Properties.Resources.CsharpLove_Button;
             this.CsharpLevel.Font = new System.Drawing.Font("Snap ITC", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.CsharpLevel.ForeColor = System.Drawing.Color.Maroon;
             this.CsharpLevel.Location = new System.Drawing.Point(398, 150);
             this.CsharpLevel.Name = "CsharpLevel";
             this.CsharpLevel.Size = new System.Drawing.Size(174, 54);
             this.CsharpLevel.TabIndex = 1;
-            this.CsharpLevel.Text = "C# LOVE";
             this.CsharpLevel.UseVisualStyleBackColor = true;
             this.CsharpLevel.Click += new System.EventHandler(this.CsharpLevel_Click);
             this.CsharpLevel.MouseHover += new System.EventHandler(this.CsharpLevel_MouseHover);
             // 
             // Labirint2
             // 
+            this.Labirint2.BackgroundImage = global::FarthorlPacMan.Properties.Resources.Labirint2_Button;
             this.Labirint2.Font = new System.Drawing.Font("Snap ITC", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Labirint2.ForeColor = System.Drawing.Color.Maroon;
             this.Labirint2.Location = new System.Drawing.Point(398, 90);
             this.Labirint2.Name = "Labirint2";
             this.Labirint2.Size = new System.Drawing.Size(174, 54);
             this.Labirint2.TabIndex = 2;
-            this.Labirint2.Text = "LABIRINT 2";
             this.Labirint2.UseVisualStyleBackColor = true;
             this.Labirint2.Click += new System.EventHandler(this.button1_Click);
             this.Labirint2.MouseHover += new System.EventHandler(this.Labirint2_MouseHover);
             // 
             // EuroLevel
             // 
+            this.EuroLevel.BackgroundImage = global::FarthorlPacMan.Properties.Resources.Euro2016_Button;
             this.EuroLevel.Font = new System.Drawing.Font("Snap ITC", 15F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.EuroLevel.ForeColor = System.Drawing.Color.Maroon;
             this.EuroLevel.Location = new System.Drawing.Point(398, 210);
             this.EuroLevel.Name = "EuroLevel";
             this.EuroLevel.Size = new System.Drawing.Size(174, 54);
             this.EuroLevel.TabIndex = 3;
-            this.EuroLevel.Text = "Euro 2016";
             this.EuroLevel.UseVisualStyleBackColor = true;
             this.EuroLevel.Click += new System.EventHandler(this.button2_Click);
             this.EuroLevel.MouseHover += new System.EventHandler(this.EuroLevel_MouseHover);
