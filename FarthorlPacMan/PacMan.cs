54281f34dc610585c3304ad6d228498eecd6184e
DonchoPatronski <donstz@yahoo.com>
2016-07-06 03:07:04 +0300

    Bug to pass throught walls. Fixed

--- FarthorlPacMan/PacMan.cs
+++ FarthorlPacMan/PacMan.cs
@@ -130,6 +130,9 @@ namespace FarthorlPacMan
             if (Engine.isDirectionChanged(base.MovedDirection))
             {
                 base.MovedDirection = Engine.MoveDirection;
+                // next we try to return to the same quadrant we have been. This prevent bug to pass throught wals.
+                string[] quadrantToMove = Engine.GetQuadrantElements(base.PositionQuadrantX, base.PositionQuadrantY);
+                TryMoveThere(quadrantToMove, base.MovedDirection);
             }
 
             switch (base.MovedDirection)
