f66ebad1b4e5b305ca241600ce26855e7d3db75a
IvayloKodov <Ivailo_Kodov@abv.bg>
2016-07-07 09:59:03 +0300

    Small changes

---
+++ FarthorlPacMan/Fruits/Fruit.cs
@@ -0,0 +1,46 @@
+﻿using System.Drawing;
+
+namespace FarthorlPacMan
+{
+    public abstract class Fruit
+    {
+        protected Graphics GraphicsFruit;
+        protected int QuadrantDimension = Global.QuadrantSize;
+        public int fruitPositionX;
+        public int fruitPositionY;
+        public int fruitWidth = 35;
+        public int fruitHeigt = 35;
+
+        protected Fruit(int x, int y, Graphics graphicsFruit)
+        {
+            this.FruitPositionX = x;
+            this.FruitPositionY = y;
+            this.GraphicsFruit = graphicsFruit;
+        }
+
+        public Image Image { get; protected set; }
+
+        public int FruitPositionX
+        {
+            get { return this.fruitPositionX; }
+            set
+            {
+                this.fruitPositionX = value;
+            }
+        }
+        public int FruitPositionY
+        {
+            get { return fruitPositionY; }
+            set
+            {
+                fruitPositionY = value;
+            }
+        }
+
+        public virtual void DrawFruit()
+        {
+        }
+
+        public abstract void ActivatePowerup();
+    }
+}
